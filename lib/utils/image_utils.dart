52a4b8627efda5bcf50648b2dc22b920b72bb25f
Xiang <g060606770@gmail.com>
2024-10-07 01:12:12 +0800

    Add image compression

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -1,6 +1,4 @@
-import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageUtils {
@@ -8,15 +6,18 @@ class ImageUtils {
 
   static Future<Uint8List?> pickImage() async {
     final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    final XFile? image = await picker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 500,
+      maxHeight: 500,
+      imageQuality: 60,
+      requestFullMetadata: false,
+    );
 
     if (image != null) {
       Uint8List? picture = await image.readAsBytes();
-      if (kIsWeb) {
-        Image.network(image.path);
-      } else {
-        Image.file(File(image.path));
-      }
+      debugPrint('picture_lengthInBytes:${picture.lengthInBytes}');
+
       return picture;
     } else {
       debugPrint("沒有選擇圖片");
